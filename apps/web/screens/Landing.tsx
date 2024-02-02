dcc9f8f63c56c56eaed4bdec75ad69e73168b7de
Govinda04 <govindakocharekar@gmail.com>
2024-02-02 22:08:19 +0530

    changes

--- apps/web/screens/Landing.tsx
+++ apps/web/screens/Landing.tsx
@@ -24,6 +24,7 @@ export async function Landing() {
       <div className="flex justify-center pt-4">
         <div className="text-white text-4xl p-2 max-w-screen-md">Learning Paths</div>
       </div>
+
       <div>
         <ul className="p-8 md:20 grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-2">
           {tracks.map((t: Track) => (
